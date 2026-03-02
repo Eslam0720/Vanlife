d1b84401ac0d28a17f3d074333f78b17cfb8bc9c
Soly <solystorm1@gmail.com>
2026-03-02 05:04:47 +0200

    Create Home and About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {  } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function About() {
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='grid justify-items-center-safe '>
+      <div className='text-center my-5'>
+        <img className='w-screen h-120' src='./public/about.png' />
+        <h1 className='text-bold mt-3 text-3xl'>Don't squeeze in a sedan when you could relax in a van.</h1>
+        <h3 className='text-bold mt-4'>
+          Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
+          (Hitch costs extra 😉) <br />
+          Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.
+        </h3>
+        <div className='bg-orange-300 grid justify-items-start p-5 justify-self-center w-160 rounded-xl mb-5'>
+        <h2 className='text-2xl text-extrabold my-2'>Your destination is waiting.</h2>
+        <h2 className='text-2xl text-extrabold my-2'>Your van is ready.</h2>
+        <button className='bg-black text-white text-center rounded-2xl p-4 mt-3'>Explore our vans</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div>
     </>
   )
 }
+
+function Home() {
+  return(
+    <>
+        <div className="home-container h-screen">
+            <h1>You got the travel plans, we got the travel vans.</h1>
+            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <Link to="vans">Find your van</Link>
+        </div>
+    </>
+  )
+}
+
 
 export default App
